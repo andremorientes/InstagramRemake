7ab851cf7a7a424c8da80bc3e982fb8102b5286a
André Morientes <59062594+andremorientes@users.noreply.github.com>
2024-01-24 11:04:32 +0200

    ToolBar Constumizada e com Menus

--- app/src/main/java/co/tiagoaguiar/course/instagram/main/view/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/main/view/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsetsController
 import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import co.tiagoaguiar.course.instagram.R
 
@@ -17,6 +18,12 @@ class MainActivity : AppCompatActivity() {
         window.insetsController?.setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
             WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
         window.statusBarColor= ContextCompat.getColor(this,R.color.gray)
+
+        val toolbar= findViewById<Toolbar>(R.id.main_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_insta_camera)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title=""
 
     }
 }
